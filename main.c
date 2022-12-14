cfb80cc91ffb5a25c037e692e96041e379742c3f
wzwg <31052511+wzwg@users.noreply.github.com>
2022-12-14 04:11:48 +0000

    2

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int Add(int a, int b)
+{
+    return a + b;
+}
+
+void Say()
+{
+    printf("Hello\r\n");
+}
+
+int main(int argc, char **argv)
+{
+    printf("hw\r\n");
+}
---
+++ public.h
@@ -0,0 +1,7 @@
+#ifndef __PUBLIC_H__
+#define __PUBLIC_H__
+
+int Add(int a, int b);
+void Say();
+
+#endif 
